ae1a7b0c1b749d0ade6959aee4dd319a40842e1e
darkdebo <50295688+darkdebo@users.noreply.github.com>
2019-09-24 10:25:49 +0530

    Update config.js

--- bot/config.js
+++ bot/config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'key',
-  consumer_secret:      'key',
-  access_token:         'key',
-  access_token_secret:  'key'
+  consumer_key:         'iAA8CQ8qQPtUUxBbY4Me4H0e3',
+  consumer_secret:      'hAB4vZWlLeiq1YDVgaU9WvlRTyq3TXskDyc2A4e4PjtqEVRDZh',
+  access_token:         '1175597608124862466-KMFClMbTTaqxgeRUxrM5snFWaJcc4V',
+  access_token_secret:  'PkIFRtTFKRh7aRZqvYoTt1VT6s9HkaA3abkSgq2FjzWGO'
 }
